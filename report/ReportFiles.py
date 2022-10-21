6347d623d3b82da89b80002fb85e3f169e321bc2
mhrnciar <matej.hrnciar2@gmail.com>
2022-10-21 09:00:40 +0200

    Fixed problems with IDs when the name of the motif contained spaces, colons, or commas

--- report/ReportFiles.py
+++ report/ReportFiles.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shutil
 
 from collections import Counter
@@ -687,7 +688,8 @@ def write_report(report_dir, motifs, output_dir, quiet=False):
 
         for motif in sorted(motifs, key=lambda x: x['full_name']):
             m = motif['full_name']
-            tabs.append(report.html_templates.motif_summary.format(motif_name=m, motif_tg=m, motif_tgf=m, table='\n'.join(rows[m]), motifs='\n'.join(ms[m])))
+            motif_clean = re.sub(r'[\s+:,]', '', m)
+            tabs.append(report.html_templates.motif_summary.format(motif_name=motif_clean, motif_tg=motif_clean, motif_tgf=motif_clean, table='\n'.join(rows[m]), motifs='\n'.join(ms[m])))
 
         f.write(custom_format(template, motifs='\n'.join(tabs)))
 
